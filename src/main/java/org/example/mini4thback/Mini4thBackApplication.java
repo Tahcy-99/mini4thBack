0af2d8648d8a2f5b4395c0288723c2f156138e48
Tahcy-99 <sjae999@gmail.com>
2026-02-24 09:20:22 +0900

    [Init] Spring Boot Web, Spring Security, JPA, MariaDB 클라이언트 설치 및 세팅

---
+++ src/main/java/org/example/mini4thback/Mini4thBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.mini4thback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mini4thBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mini4thBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/mini4thback/Mini4thBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mini4thback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mini4thBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
